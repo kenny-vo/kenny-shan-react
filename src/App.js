9dfdc3f607ba5c85303c31d1f356f0001a088352
Kenny Vo <kennyvo91@gmail.com>
2018-05-28 16:55:25 -0500

    countdown working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import NavbarHeader from './components/Navbar/Navbar';
+import NavbarHeader from './components/Navbar';
+import Countdown from './components/Countdown';
 import './App.css';
 import './App.scss';
 
 class App extends Component {
   render() {
+    const currentDate = new Date();
+    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
     return (
       <div className="wrapper">
         {/* <NavbarHeader /> */}
@@ -12,12 +15,16 @@ class App extends Component {
           <div className="container">
             <div className="header-text-box col-xs-12 col-md-4">
               <h1 className="header-title">
-                SHAN &amp;	 KENNY
+                SHAN &amp; KENNY
+                <Countdown date={`2019-10-18T00:00:00`} />
               </h1>
               <br/>
             <h1 className="header-text">
                 SAVE THE DATE
               </h1>
+
+              {/* <Countdown date={`2019-12-24T00:00:00`} /> */}
+
             </div>
           </div>
         </section>
